2eeb0fc906cfcca54415eb6ab0ade4c7591acbc3
Victor Vieira <victor.h.souza.vieira@gmail.com>
2024-12-26 01:47:37 -0300

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.guava)
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter("5.11.1")
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "org.example.AppKt"
+}
