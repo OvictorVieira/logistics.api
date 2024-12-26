1db83f144bd1030999d322289ad07fc54c96895c
Victor Vieira <victor.h.souza.vieira@gmail.com>
2024-12-26 20:18:49 -0300

    fix: Creates database migrations

---
+++ app/src/main/resources/db/migration/V1__create_table_users.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS logistics.users (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(45) NOT NULL
+);
---
+++ app/src/main/resources/db/migration/V2__create_table_products.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS logistics.products (
+    id BIGINT PRIMARY KEY,
+    value DECIMAL(12, 2) NOT NULL
+);
