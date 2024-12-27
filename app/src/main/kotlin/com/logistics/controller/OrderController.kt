efb593a97822088c5e68751f08ea2104e3a564c5
Victor Vieira <victor.h.souza.vieira@gmail.com>
2024-12-27 19:59:01 -0300

    feat: Added filters to endpoint

--- app/src/main/kotlin/com/logistics/controller/OrderController.kt
+++ app/src/main/kotlin/com/logistics/controller/OrderController.kt
@@ -4,11 +4,14 @@ import com.logistics.dto.out.ApiResponse
 import com.logistics.dto.out.UserOrdersResponse
 import com.logistics.service.OrderService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/api/orders")
@@ -16,12 +19,16 @@ class OrderController @Autowired constructor(
     private val orderService: OrderService
 ) {
 
-    @GetMapping("/all")
-    fun getAllOrdersGroupedByUser(): ResponseEntity<ApiResponse<List<UserOrdersResponse>>> {
+    @GetMapping("/filter")
+    fun getOrdersWithFilters(
+        @RequestParam(required = false) userId: Long?,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) fromDate: LocalDate?,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) toDate: LocalDate?
+    ): ResponseEntity<ApiResponse<List<UserOrdersResponse>>> {
         return try {
-            val allOrders = orderService.getAllOrdersGroupedByUser()
+            val allOrders = orderService.getOrdersWithFilters(userId, fromDate, toDate)
 
-            val response = ApiResponse( success = true, message = "Get all orders successfully", data = allOrders)
+            val response = ApiResponse( success = true, message = "Filter orders successfully", data = allOrders)
 
             ResponseEntity(response, HttpStatus.OK)
         } catch (e: Exception) {
--- app/src/main/kotlin/com/logistics/repository/OrderRepository.kt
+++ app/src/main/kotlin/com/logistics/repository/OrderRepository.kt
@@ -3,25 +3,33 @@ package com.logistics.repository
 import com.logistics.domain.Order
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 
 @Repository
 interface OrderRepository : JpaRepository<Order, Long> {
+
     @Query(
-        """ 
+        """
         SELECT 
-            o.order_id,
-            o.user_id,
-            u.name,
-            o.product_id,
-            p.value AS product_value,
-            o.purchase_date
-        FROM
-            logistics.orders o 
-        JOIN logistics.users u ON o.user_id = u.user_id
-        JOIN logistics.products p ON o.product_id = p.product_id
+            o.order_id, o.user_id, u.name, p.product_id, p.value AS product_value, o.purchase_date
+        FROM 
+            logistics.orders o
+        JOIN 
+            logistics.users u ON o.user_id = u.user_id
+        JOIN 
+            logistics.products p ON o.product_id = p.product_id
+        WHERE  
+            (:userId IS NULL OR o.user_id = :userId )
+          AND ( Cast(:fromDate AS DATE) IS NULL OR o.purchase_date >= Cast(:fromDate AS DATE) )
+          AND ( Cast(:toDate AS DATE) IS NULL OR o.purchase_date <= Cast(:toDate AS DATE) )
         """,
         nativeQuery = true
     )
-    fun findAllOrdersWithUserAndProduct(): List<Map<String, Any>>
+    fun findOrdersWithFilters(
+        @Param("userId") userId: Long?,
+        @Param("fromDate") fromDate: LocalDate?,
+        @Param("toDate") toDate: LocalDate?
+    ): List<Map<String, Any>>
 }
