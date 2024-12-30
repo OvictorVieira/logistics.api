333a57715160062d7715271545404127094af51c
Victor Vieira <victor.h.souza.vieira@gmail.com>
2024-12-30 17:44:33 -0300

    feat: Adds mockito kotlin dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
 }
 
 testing {
