4ed1ccac0a2f15d8007a8d7dd197802641616f42
Victor Vieira <victor.h.souza.vieira@gmail.com>
2024-12-30 10:43:40 -0300

    feat: Adds test database dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 testing {
